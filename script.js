84e80e9d75165baccad47a5e7d6b1872166f8da5
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-03-06 10:40:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your JS code here. If required.
+let btn = document.getElementById("btn");
+let output = document.getElementById("output");
+
+btn.addEventListener("click", () => {
+    let num = Number(document.getElementById("ip").value);
+
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(num);
+        }, 2000);
+    })
+        .then((res) => {
+            output.textContent = res;
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    let doubled = res * 2;
+                    output.textContent = doubled;
+                    resolve(doubled);
+                }, 1000);
+            });
+        })
+        .then((res) => {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    let sub = res - 3;
+                    output.textContent = sub;
+                    resolve(sub);
+                }, 1000);
+            });
+        })
+        .then((res) => {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    let div = res / 2;
+                    output.textContent = div;
+                    resolve(div);
+                }, 1000);
+            });
+        })
+        .then((res) => {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                   output.textContent = res;
+                }, 1000);
+            });
+        });
+
+});
